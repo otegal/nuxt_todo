9b3d8d473fcfc077bbbc5151edf40ae09b19933a
h-hiroki <gyouzakuuka@gmail.com>
2019-09-30 00:38:54 +0900

    get data by axios

--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,23 @@
     <h1>
       Hello, World!
     </h1>
+    <ul>
+      <li v-for="user in users" v-bind:key='user.id'>
+        {{ user }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData() {
+    const { data } = await axios.get('https://api.myjson.com/bins/yzzof')
+    return {
+      users: data
+    }
+  }
+}
+</script>
