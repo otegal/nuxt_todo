49730f32ce23ed1ed20e21214dd76ea9893f4abd
h-hiroki <gyouzakuuka@gmail.com>
2019-09-30 01:24:11 +0900

    create todo list

---
+++ components/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="todoList">
+    <h2>TodoList</h2>
+    <ul>
+      <li v-for="todo in todos" v-bind:key="todo.id">
+        {{ todo.text }}/done:{{ todo.done }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    todos: Array
+  }
+}
+</script>
+
+<style scoped>
+#todoList {
+  background-color: #aae
+}
+</style>
