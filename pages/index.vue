ed59f8d589ef74c0b7b06cabe0c3c1a2d80c34f5
h-hiroki <gyouzakuuka@gmail.com>
2019-09-30 00:45:01 +0900

    change list view

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,55 @@
 <template>
   <div>
-    <h1>
-      Hello, World!
-    </h1>
-    <ul>
-      <li v-for="user in users" v-bind:key='user.id'>
-        {{ user }}
-      </li>
-    </ul>
+    <table>
+      <caption>好きなものリスト</caption>
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th>likes</th>
+        </tr>
+      </thead>
+      <tbody v-for="user in users" v-bind:key="user.id">
+        <tr>
+          <td>{{ user.id }}</td>
+          <td>{{ user.name }}</td>
+          <td>
+            <ul>
+              <li v-for="(item, index) in user.like" v-bind:key="index">
+                {{ item }}
+              </li>
+            </ul>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+
+<style scoped>
+table {
+  width: 100%;
+}
+
+tbody:nth-child(odd) {
+  background: #eee;
+}
+
+ul {
+  padding: 0;
+  margin: 5px 0;
+}
+
+li {
+  display: inline;
+  margin: 0 5px;
+  padding: 5px;
+  background-color: #ccc;
+  border-radius: 20px;
+}
+</style>
+
 
 <script>
 import axios from 'axios'
